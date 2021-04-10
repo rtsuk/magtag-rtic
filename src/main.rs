9ae3fa9e39973ee94020a86340cda00119e01148
Rob Tsuk <rob@tsuk.com>
2021-04-10 07:46:01 -0700

    Tweaks for 6
    # Conflicts:
    #	Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 //! examples/init.rs
 
 #![deny(unsafe_code)]
-//#![deny(warnings)]
+#![deny(warnings)]
 #![no_main]
 #![no_std]
 
 use panic_semihosting as _;
-//use feather_m4 as hal;
+use feather_m4::prelude::_atsamd_hal_embedded_hal_digital_v2_OutputPin;
 
-#[rtic::app(device = atsamd51j, peripherals = true)]
+#[rtic::app(device = feather_m4::pac, peripherals = true)]
 mod app {
     use cortex_m_semihosting::hprintln;
 
@@ -17,9 +17,8 @@ mod app {
         hprintln!("init").unwrap();
 
         let _core = cx.core;
-        let _device = cx.device;
-        let peripherals = atsamd51j::Peripherals::take().unwrap();
-        let mut pins = feather_m4::Pins::new(peripherals.PORT);
+        let device = cx.device;
+        let mut pins = feather_m4::Pins::new(device.PORT);
         let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
         red_led.set_high().unwrap();
 
